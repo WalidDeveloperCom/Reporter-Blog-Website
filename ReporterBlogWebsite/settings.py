1a540e906cab3c9aca4f00dabaa3339260f403cc
MD. Walid <hi@waliddeveloper.com>
2025-02-13 01:57:23 +0600

    static and media

--- ReporterBlogWebsite/settings.py
+++ ReporterBlogWebsite/settings.py
@@ -118,10 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+# The URL for accessing media files
+MEDIA_URL = '/media/'
+
+# The location where media files will be stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
