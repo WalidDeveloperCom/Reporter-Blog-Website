9a5c5d113cbf0bb9784a309131be3d010cf66e29
MD. Walid <hi@waliddeveloper.com>
2025-02-14 00:50:58 +0600

    new model.

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 from . models import Post
 
 # def index(request):
@@ -8,3 +9,7 @@ from . models import Post
 def BlogPost(request):
     posts = Post.object.all()
     return render(request, 'blog/index.html', {"posts": post})
+
+def latest_post(request):
+    latest_post = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date').first()
+    return render(request, 'blog/latest_post.html', {'latest_post': latest_post})
