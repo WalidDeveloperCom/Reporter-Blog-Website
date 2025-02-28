7ab767d3c6ff950b1af14aff54b26c8a879496e5
MD. Walid <hi@waliddeveloper.com>
2025-02-28 12:55:27 +0600

    contact

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('latest/', views.latest_post, name='latest_post'),
     path('post/<int:post_id>/', views.post_details, name='post_details'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.db.models import Count
 from django.utils import timezone
 from .models import Post, Category
+
 
 def index(request):
     categories = Category.objects.annotate(post_count=Count('post'))
@@ -20,6 +22,10 @@ def latest_post(request):
     latest_post = Post.objects.filter(created_at__lte=timezone.now()).order_by('-created_at').first()
     return render(request, 'blog/latest_post.html', {'latest_post': latest_post})
 
+def recommended(request):
+    recommended = Post.objects.filter(created_at__lte=timezone.now()).order_by('-created_at').first()
+    return render(request, 'blog/recommended.html', {'recommended': recommended})
+
 def post_details(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog/post_details.html', {'post': post})
@@ -28,3 +34,11 @@ def category_list(request):
     categories = Category.objects.all()
     context = {'categories': categories}
     return render(request, 'blog/category_list.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'blog/about.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'blog/contact.html', context)
