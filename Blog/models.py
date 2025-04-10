02e0c63d91c1c4edfd2c538c0ad39753550ab5f3
MD. Walid <hi@waliddeveloper.com>
2025-04-10 10:52:22 +0600

    complete

--- Blog/models.py
+++ Blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
-from ckeditor.fields import RichTextField
+from ckeditor.fields import RichTextField # type: ignore
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
